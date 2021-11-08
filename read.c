6b6b896089f4c2c8c306fbc51a6e635d16b23dc0
doragon_ <lung.trabajos@gmail.com>
2021-11-07 23:29:03 -0300

    read a file and convert the line from char*-long

---
+++ read.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    long int value;
+
+    fp = fopen("Values/values_0.txt", "r");
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+        value = atol(line);;
+        printf("%li\n", value);
+    }
+
+    fclose(fp);
+}
